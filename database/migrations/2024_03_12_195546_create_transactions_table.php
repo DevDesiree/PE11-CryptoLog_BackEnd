1f896f70e59147ec9f2e7900e6281c3ba106f1e3
Desiree <desireesanchezdev@gmail.com>
2024-03-12 21:09:49 +0100

    Created transactions table

---
+++ database/migrations/2024_03_12_195546_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('coin_id');
+            $table->foreign('coin_id')->references('id')->on('coins');
+            $table->tinyInteger('quantity');
+            $table->double('price_buy', 8, 2);
+            $table->double('amount', 8, 2);
+            $table->double('actual_price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
