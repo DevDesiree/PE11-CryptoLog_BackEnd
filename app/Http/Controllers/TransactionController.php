5b0ea05931b979c8674595013dcbb78bb2602eda
Desiree <desireesanchezdev@gmail.com>
2024-03-14 14:58:49 +0100

    Created TransactionController and crud functions

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $transaction = Auth::user()->transaction;
+        return response()->json($transaction);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'coin_id' => 'required|exists:coins,id',
+                'quantity' => 'required|numeric',
+                'price_buy' => 'required|numeric',
+                'amount' => 'required|numeric',
+                'actual_price' => 'required|numeric',
+            ]);
+
+            $user = Auth::user();
+
+            $transaction = Transaction::create([
+                'user_id' => $user->id,
+                'coin_id' => $request->coin_id,
+                'quantity' => $request->quantity,
+                'price_buy' => $request->price_buy,
+                'amount' => $request->amount,
+                'actual_price' => $request->actual_price,
+            ]);
+            return response()->json([
+                'message' => 'La transacción se ha realizado correctamente',
+                'transaction' => $transaction,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al realizar la transacción: ' . $e->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $request->validate([
+                'quantity' => 'required|numeric',
+                'actual_price' => 'required|numeric',
+                'amount' => 'required|numeric',
+            ]);
+
+            $transaction = Transaction::findOrFail($id);
+
+            if ($transaction->user_id != Auth::id()) {
+                return response()->json(['message' => 'No tienes permiso para actualizar esta transacción'], 403);
+            }
+
+            $transaction->update($request->all());
+
+            return response()->json(['message' => 'La transacción se ha actualizado correctamente', 'transaction' => $transaction], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al actualizar la transacción: ' . $e->getMessage()], 500);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $transaction = Transaction::findOrFail($id);
+            $transaction->delete();
+
+            return response()->json(['message' => 'La transacción se ha eliminado correctamente'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al eliminar la transacción: ' . $e->getMessage()], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,5 +25,8 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/create-transaction', [TransactionController::class, 'store']);
+    Route::put('/update-transaction/{id}', [TransactionController::class, 'update']);
+    Route::delete('/delete-transaction/{id}', [TransactionController::class, 'destroy']);
 });
