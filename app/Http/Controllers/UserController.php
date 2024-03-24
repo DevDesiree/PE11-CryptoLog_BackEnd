4de6698bc903908de7149f06ddd4f69995af05e3
Desiree <desireesanchezdev@gmail.com>
2024-03-24 19:48:52 +0100

    Added autentication user on UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Historical;
 use Illuminate\Http\Request;
+use App\Models\Historical;
 use Illuminate\Support\Facades\Auth;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
@@ -11,56 +11,75 @@ class UserController extends Controller
 {
     public function index()
     {
-        try {
-            $user = Auth::user();
+        if (Auth::user()) {
+            try {
+                $user = Auth::user();
 
-            $userData = [
-                'name' => $user->name,
-                'email' => $user->email,
-                'avatar' => $user->avatar,
-            ];
+                $userData = [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'avatar' => $user->avatar,
+                ];
 
-            return response()->json(['user' => $userData], 200);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Error al obtener los datos del usuario: ' . $e->getMessage()], 500);
+                return response()->json(['user' => $userData], 200);
+            } catch (\Exception $e) {
+                return response()->json(['error' => 'Error al obtener los datos del usuario: ' . $e->getMessage()], 500);
+            }
         }
     }
 
     public function updateProfile(Request $request)
     {
-        try {
-            $user = $request->user();
+        if (Auth::user()) {
+            try {
+                $user = $request->user();
 
-            if ($request->hasFile('avatar')) {
-                $avatarPath = $request->file('avatar')->getRealPath();
-                $uploadedFile = Cloudinary::upload($avatarPath, [
-                    'folder' => 'avatars',
-                ]);
+                // Obtiene los datos del formulario
+                $formData = $request->only(['name', 'email']);
 
-                $user->avatar = $uploadedFile->getSecurePath();
+                // Verifica si se ha cargado un archivo
+                if ($request->hasFile('avatar')) {
+                    $avatar = $request->file('avatar');
+
+                    // Carga la imagen en Cloudinary
+                    $uploadedFile = Cloudinary::upload($avatar->getRealPath(), [
+                        'folder' => 'avatars',
+                    ]);
+
+                    // Obtiene la URL y el public_id de la imagen cargada en Cloudinary
+                    $avatarUrl = $uploadedFile->getSecurePath();
+                    $publicId = $uploadedFile->getPublicId();
+
+                    // Actualiza el perfil del usuario con la URL de la imagen y el public_id
+                    $formData['avatar'] = $avatarUrl;
+                    $formData['public_id'] = $publicId;
+                }
+
+                // Actualiza el perfil del usuario
+                $user->update($formData);
+
+                return response()->json(['message' => 'Perfil actualizado correctamente', 'user' => $user], 200);
+            } catch (\Exception $e) {
+                return response()->json(['error' => 'Error al actualizar el perfil: ' . $e->getMessage()], 500);
             }
-
-            $user->update($request->all());
-
-            return response()->json(['message' => 'Perfil actualizado correctamente', 'user' => $user], 200);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Error al actualizar el perfil: ' . $e->getMessage()], 500);
         }
     }
 
     public function getHistoricals(Request $request)
     {
-        try {
-            $user = $request->user();
+        if (Auth::user()) {
+            try {
+                $user = $request->user();
 
-            if ($user) {
-                $historicals = Historical::latest()->get();
-                return response()->json($historicals);
-            } else {
-                return response()->json(['message' => 'Usuario no autenticado.'], 401);
+                if ($user) {
+                    $historicals = Historical::latest()->get();
+                    return response()->json($historicals);
+                } else {
+                    return response()->json(['message' => 'Usuario no autenticado.'], 401);
+                }
+            } catch (\Exception $e) {
+                return response()->json(['message' => 'Error al obtener los datos históricos.'], 500);
             }
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'Error al obtener los datos históricos.'], 500);
         }
     }
 }
